1ffe1e62b0ff2933c58b33ec1cef80f3d9bca799
rifthd <37347413+rifthd@users.noreply.github.com>
2018-03-13 20:10:25 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,73 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import chalk
+
+bot = commands.Bot(command_prefix='*')
+
+print (discord.__version__)
+
+@bot.event
+async def on_ready():
+    print ("Ready when you are xd")
+    print ("I am running on " + bot.user.name)
+    print ("With the ID: " + bot.user.id)
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say(":ping_pong: ping!! xSSS")
+    print ("user has pinged")
+
+@bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def serverinfo(ctx):
+    embed = discord.Embed(name="{}'s info".format(ctx.message.server.name), description="Here's what I could find.", color=0x00ff00)
+    embed.set_author(name="RIFT HD IS MY BOSS")
+    embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
+    embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
+    embed.add_field(name="Members", value=len(ctx.message.server.members))
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+@commands.has_role("ðŸ˜‚ðŸ‘Œ Friends")
+async def kick(ctx, user: discord.Member):
+    await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
+    await bot.kick(user)
+
+@bot.command(pass_context=True)
+async def embed(ctx):
+    embed = discord.Embed(title="test", description="my name jeff", color=0x00ff00)
+    embed.set_footer(text="this is a footer")
+    embed.set_author(name="Will Ryan of DAGames")
+    embed.add_field(name="This is a field", value="no it isn't", inline=True)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def loser(ctx):
+    await bot.say("nah you")
+    print ("user has trolled")
+
+@bot.command(pass_context=True)
+async def WHOISYOURBOSS(ctx):
+    await bot.say("RIFT HD#7379 IS MY BOSS")
+    print ("user has trolled")
+
+@bot.command(pass_context=True)
+async def WhoAreYou(ctx):
+    await bot.say("I'm Jäger from the Game Rainbow Six Siege http://www.shooter-szene.de/wp-content/uploads/2015/12/Siege_J%C3%A4ger_3.jpg")
+    print ("user has trolled")
+
+bot.run("NDIzMTczNzY1NzAzMTM5MzU4.DYmftg.yzG_jSRoPhF9mFY61d8H9K3_V8E")
